b80469fae08f009311c440b27ae36370694db00e
deniseglaus <denise.glaus@gmail.com>
2022-07-06 18:52:36 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ export default App;
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Denise and is{" "}
-        <a
-          href="https://github.com/deniseglaus/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-source on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Denise and is{" "}
+          <a
+            href="https://github.com/deniseglaus/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-source on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
