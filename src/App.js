0501b011caec96e4842a09ef44d04f53536e0588
deniseglaus <denise.glaus@gmail.com>
2022-07-06 18:04:42 +0200

    added GitHub link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
 import React from "react";
 
-function App() {
-  return <div className="App">Weather App</div>;
-}
-
 export default App;
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Denise and is{" "}
+        <a
+          href="https://github.com/deniseglaus/react-weather-app"
+          target="_blank"
+        >
+          open-source on GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
