c527114932c8fcf65346fe6e840e8a179ac42873
deniseglaus <denise.glaus@gmail.com>
2022-07-16 16:49:02 +0200

    corrected errors regarding axios

--- src/FormatedDate.js
+++ src/FormatedDate.js
@@ -10,7 +10,7 @@ export default function FormatedDate(props) {
     "Friday",
     "Saturday",
   ];
-  let day = [props.date.getDay()];
+  let day = days[props.date.getDay()];
   let hours = props.date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
