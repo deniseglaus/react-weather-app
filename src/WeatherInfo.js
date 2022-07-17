c6c17015c05d532a8ed6e9c583fe00c1d3751d29
deniseglaus <denise.glaus@gmail.com>
2022-07-17 17:14:28 +0200

    corrected errors

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,24 +4,24 @@ import FormatedDate from "./FormatedDate";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{weatherData.city}</h1>
+      <h1>{props.data.city}</h1>
       <ul>
         <li>
           <FormatedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">{weatherData.description}</li>
+        <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
             <img
-              src={weatherData.iconUrl}
-              alt={weatherData.description}
+              src={props.data.iconUrl}
+              alt={props.data.description}
               className="float-left"
             />
             <div className="float-right">
               <span className="temperature">
-                {Math.round(weatherData.temperature)}
+                {Math.round(props.data.temperature)}
               </span>
               <span className="unit">°C</span>
             </div>
@@ -29,8 +29,8 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Humditiy: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} km/h</li>
+            <li>Humditiy: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
